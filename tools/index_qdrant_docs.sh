8cd7d08e8bd182ce7a2ecce6450db11868f368e4
Andrey Vasnetsov <andrey@vasnetsov.com>
2023-03-20 15:23:39 +0100

    add CI indexing

---
+++ tools/index_qdrant_docs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="$(pwd)/$(dirname "$0")/../"
+
+cd $(mktemp -d)
+
+git clone git@github.com:qdrant/docs.git
+cd docs
+
+LATEST_VERSION=$(ls -1 qdrant/ | sort -V | tail -n 1)
+
+DOCS_DIR="$(pwd)/qdrant/${LATEST_VERSION}/"
+
+cd ${PROJECT_ROOT}
+
+python -m markdown_search.index --docs-dir ${DOCS_DIR} --url-prefix 'https://qdrant.tech/documentation/'
+
